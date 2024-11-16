3e09f7706e3c7aa3961b66d96fcc6986953daf29
Kavya Gupta <165313930+kavyagupta3011@users.noreply.github.com>
2024-11-16 15:25:31 +0530

    ExistingQuizzes.java added

---
+++ ExistingQuizzes.java
@@ -0,0 +1,17 @@
+
+public class ExistingQuizzes extends Quiz
+{
+    
+    private int score;
+
+    //add constructotr 
+    
+    //getter setter
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score=score;
+    }
+}
